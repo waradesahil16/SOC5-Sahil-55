b03fa3f70c34b3a5553537e309d95389a58baef8
Sahil Warade <waradesahil29@gmail.com>
2026-09-08 13:24:54 +0530

    Add files via upload

---
+++ stu.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class MyClass
+{
+public:
+int MyNum;
+string Mystring;
+};
+int main()
+{
+Myclass c;
+c.MyNum=10;
+c.Mystring = "Hello";
+cout<<"c.MyString"<<",c.MyNum"<< endl;
+return 0;
+}
